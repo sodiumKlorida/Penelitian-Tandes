88145253f39aade9dd6f0562eb27714c5420e881
eca <116784250+sodiumKlorida@users.noreply.github.com>
2025-08-27 11:53:30 +0700

    first push eca

--- cuaca/app/app.py
+++ cuaca/app/app.py
@@ -1,17 +1,3 @@
-# from flask import Flask, render_template
-# # from BMKG import get_cuaca
-
-# app = Flask(__name__)
-
-# # call html
-# @app.route("/")
-# def home():
-#     # cuaca = get_cuaca()
-#     return render_template("dashboard.html")
-
-# if __name__ == "__main__":
-#     app.run(debug=True)
-
 from flask import Flask, render_template
 from routes.BMKG import bmkg_bp
 from routes.AQICN import aqicn_bp
--- cuaca/app/routes/AQICN.py
+++ cuaca/app/routes/AQICN.py
@@ -1,33 +1,3 @@
-# from flask import Flask, jsonify, render_template
-# import requests
-
-# # Membuat instance aplikasi Flask
-# app = Flask(__name__)
-
-# @app.route("/tandes")
-# def tandes():
-#     url = "https://api.waqi.info/feed/A420154/?token=8e6fff766a818b4aeba7e9b74e1a293da3f7c4dd"
-#     response = requests.get(url).json()
-    
-#     if response["status"] == "ok":
-#         data = {
-#             "kota": response["data"]["city"]["name"],
-#             "aqi": response["data"]["aqi"],
-#             "pm25": response["data"]["iaqi"].get("pm25", {}).get("v", None),
-#             "pm10": response["data"]["iaqi"].get("pm10", {}).get("v", None),
-#             "co": response["data"]["iaqi"].get("co", {}).get("v", None),
-#             "suhu": response["data"]["iaqi"].get("t", {}).get("v", None),
-#             "kelembapan": response["data"]["iaqi"].get("h", {}).get("v", None),
-#             "waktu": response["data"]["time"]["s"]
-#         }
-#         return jsonify(data)
-#     else:
-#         return jsonify({"error": "Data tidak ditemukan"})
-
-# # Menjalankan aplikasi Flask di mode debug
-# if __name__ == "__main__":
-#     app.run(debug=True)
-
 from flask import Blueprint, jsonify
 import requests
 
